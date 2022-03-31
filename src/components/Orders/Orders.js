115ab500b3c42db6483ff0aa40187d99a42f845c
tashfiaislamistila <tashfia.cse@gmail.com>
2022-03-30 17:55:32 -0700

    usecart by custom hook commit

--- src/components/Orders/Orders.js
+++ src/components/Orders/Orders.js
@@ -1,11 +1,14 @@
 import React, { useState } from 'react';
+import useCart from '../Hooks/useCart';
 import useProducts from '../Hooks/useProducts';
 
 const Orders = () => {
     const [products, setProducts] = useProducts();
+    const [cart, setCart] = useCart(products);
     return (
         <div>
             <h2>This is Orders:{products.length}</h2>
+            <p>Cart has :{cart.length}</p>
         </div>
     );
 };
