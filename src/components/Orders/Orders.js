26c9b9c8f964ebbab3d82ffb1294cdbf8e6b43b7
tashfiaislamistila <tashfia.cse@gmail.com>
2022-03-30 19:00:31 -0700

    reviewitem component added and give style in order page commit

--- src/components/Orders/Orders.js
+++ src/components/Orders/Orders.js
@@ -1,14 +1,25 @@
 import React, { useState } from 'react';
+import Cart from '../Cart/Cart';
 import useCart from '../Hooks/useCart';
 import useProducts from '../Hooks/useProducts';
+import ReviewItem from '../ReviewItem/ReviewItem';
 
 const Orders = () => {
     const [products, setProducts] = useProducts();
     const [cart, setCart] = useCart(products);
     return (
-        <div>
-            <h2>This is Orders:{products.length}</h2>
-            <p>Cart has :{cart.length}</p>
+        <div className='shop-container'>
+            <div className='review-item-container'>
+                {
+                    cart.map(product => <ReviewItem
+                        key={product.id}
+                        product={product}
+                    ></ReviewItem>)
+                }
+            </div>
+            <div className="cart-container">
+                <Cart cart={cart}></Cart>
+            </div>
         </div>
     );
 };
