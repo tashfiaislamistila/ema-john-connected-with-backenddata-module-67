c1a946f62a24e89bf6e79251bd392cf040cc22de
tashfiaislamistila <tashfia.cse@gmail.com>
2022-04-10 14:18:31 -0700

    some css in login page commit

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,9 +1,26 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './Login.css'
 const Login = () => {
     return (
         <div className='form-container'>
-            <h2 className='form-title'>Login</h2>
+            <div>
+                <form>
+                    <h2 className='form-title'>Login</h2>
+                    <div className='input-group'>
+                        <label htmlFor='email'>Email</label>
+                        <input type="email" name="email" id="" />
+                    </div>
+                    <div className="input-group">
+                        <label htmlFor="password">Password</label>
+                        <input type="password" name="password" id="" />
+                    </div>
+                    <input className='form-submit' type="submit" value="Login" />
+                </form>
+                <p>
+                    new to Ema-john?<Link className='form-link' to="/signup">Create an account</Link>
+                </p>
+            </div>
         </div>
     );
 };
