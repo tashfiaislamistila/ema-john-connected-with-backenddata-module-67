108e7717521f94c928cd685d71b46a9d64a6b725
tashfiaislamistila <tashfia.cse@gmail.com>
2022-04-10 15:48:30 -0700

    events added commit

--- src/components/SignUp/SignUp.js
+++ src/components/SignUp/SignUp.js
@@ -1,9 +1,54 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 
 const SignUp = () => {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [confirmPassword, setConfirmPassword] = useState('');
+    const [error, setError] = useState('');
+
+    const handleEmailBlur = event => {
+        setEmail(event.target.value);
+    }
+    const handlePasswordBlur = event => {
+        setPassword(event.target.value);
+    }
+    const handleConfirmPasswordBlur = event => {
+        setConfirmPassword(event.target.value)
+    }
+    const handleCreateUser = event => {
+        event.preventDefault();
+        if (password !== confirmPassword) {
+            setError("Your two password did not match");
+            return;
+        }
+    }
     return (
-        <div>
-            <h2>Sign Up</h2>
+        <div className='form-container'>
+            <div>
+                <form onSubmit={handleCreateUser}>
+                    <h2 className='form-title'>Sign UP</h2>
+                    <div className='input-group'>
+                        <label htmlFor='email'>Email</label>
+                        <input onBlur={handleEmailBlur} type="email" name="email" id="" required />
+                    </div>
+                    <div className="input-group">
+                        <label htmlFor="password">Password</label>
+                        <input onBlur={handlePasswordBlur} type="password" name="password" id="" required />
+                    </div>
+                    <div className="input-group">
+                        <label htmlFor="confirm-password">Confirm Password</label>
+                        <input onBlur={handleConfirmPasswordBlur} type="password" name="confirm-password" id="" required />
+                    </div>
+                    <p style={{ color: 'red' }}>
+                        {error}
+                    </p>
+                    <input className='form-submit' type="submit" value="Sign Up" />
+                </form>
+                <p>
+                    Already Have an account?<Link className='form-link' to="/login">Login</Link>
+                </p>
+            </div>
         </div>
     );
 };
