51dac5a46578345b7d29b52f96f07e287ac933b2
tashfiaislamistila <tashfia.cse@gmail.com>
2022-04-10 16:38:10 -0700

    create user with email password and navigate shop commit

--- src/components/SignUp/SignUp.js
+++ src/components/SignUp/SignUp.js
@@ -1,11 +1,15 @@
 import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
+import { useCreateUserWithEmailAndPassword } from 'react-firebase-hooks/auth';
+import auth from '../../firebase.init';
 
 const SignUp = () => {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
     const [confirmPassword, setConfirmPassword] = useState('');
     const [error, setError] = useState('');
+    const navigate = useNavigate();
+    const [createUserWithEmailAndPassword, user] = useCreateUserWithEmailAndPassword(auth)
 
     const handleEmailBlur = event => {
         setEmail(event.target.value);
@@ -16,12 +20,20 @@ const SignUp = () => {
     const handleConfirmPasswordBlur = event => {
         setConfirmPassword(event.target.value)
     }
+    if (user) {
+        navigate('/shop');
+    }
     const handleCreateUser = event => {
         event.preventDefault();
         if (password !== confirmPassword) {
             setError("Your two password did not match");
             return;
         }
+        if (password.length < 6) {
+            setError("Password must be six characters or longer");
+            return;
+        }
+        createUserWithEmailAndPassword(email, password);
     }
     return (
         <div className='form-container'>
